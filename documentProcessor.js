9a572afcfa7977bda979b2b881415ddba194090a
Andrea Salvatore <andrea.salvatore@consensys.net>
2023-03-31 01:45:02 +0200

    adds chat history support and refactoring

--- documentProcessor.js
+++ documentProcessor.js
@@ -6,29 +6,35 @@ import { HNSWLib } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from "langchain/embeddings";
 
 export async function loadAndProcessDocuments(directoryPath) {
-  const loader = new DirectoryLoader(directoryPath, {
-    ".md": (path) => new TextLoader(path),
-  });
-
-  const docs = await loader.load();
-  const splitter = new MarkdownTextSplitter();
-  const sdk_docs = [];
-
-  const processDocs = async (doc) => {
-    const text = doc.pageContent;
-    const output = await splitter.createDocuments([text], {
-      metadata: "sdk_docs",
+  try {
+    const directoryLoader = new DirectoryLoader(directoryPath, {
+      ".md": (path) => new TextLoader(path),
     });
 
-    sdk_docs.push(...output);
-  };
+    const documentLoaders = await directoryLoader.load();
+    const markdownSplitter = new MarkdownTextSplitter();
+    const allDocuments = [];
 
-  await Promise.all(docs.map(processDocs));
+    const splitDocuments = async (documentLoader) => {
+      const text = documentLoader.pageContent;
+      const documents = await markdownSplitter.createDocuments([text], {
+        metadata: "metamask_sdk_documentation",
+      });
+      allDocuments.push(...documents);
+    };
 
-  const vectorStore = await HNSWLib.fromDocuments(
-    sdk_docs,
-    new OpenAIEmbeddings()
-  );
+    await Promise.all(documentLoaders.map(splitDocuments));
 
-  return vectorStore;
+    const vectorStore = await HNSWLib.fromDocuments(
+      allDocuments,
+      new OpenAIEmbeddings()
+    );
+
+    return vectorStore;
+  } catch (error) {
+    console.error(error);
+    throw new Error(
+      "An error occurred while loading and processing documents."
+    );
+  }
 }
--- index.js
+++ index.js
@@ -1,58 +1,67 @@
-// main.js
-
 import { OpenAI } from "langchain/llms";
 import { ConversationalRetrievalQAChain } from "langchain/chains";
 import dotenv from "dotenv";
 import { loadAndProcessDocuments } from "./documentProcessor.js";
+import readline from "readline";
 
 dotenv.config();
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const { OPENAI_API_KEY } = process.env;
 
 async function main() {
-  const model = new OpenAI({
-    openAIApiKey: OPENAI_API_KEY,
-    temperature: 0.9,
-    maxTokens: 2500,
-  });
+  try {
+    const model = new OpenAI({
+      openAIApiKey: OPENAI_API_KEY,
+      temperature: 0.9,
+      maxTokens: 2500,
+    });
 
-  const vectorStore = await loadAndProcessDocuments("sdk_docs/");
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
-    vectorStore.asRetriever()
-  );
+    const vectorStore = await loadAndProcessDocuments("sdk_docs/");
 
-  // TODO: cache the `vectorStore` after processing the documents, so we don't have to redo the document loading and processing each time you ask questions.
+    // Cache the vectorStore
+    const retriever = vectorStore.asRetriever();
 
-  /* Ask it questions */
-  const questions = [
-    "How can I install MetaMask SDK on iOS?",
-    "Where can I get support?",
-    "What about React Native?",
-    "Which platform does the Unity SDK supports?",
-    "Which dependencies does the Unity SDK have?",
-    "Is there a Discord server?",
-    "What are the plans for the future develoment?",
-    "What is the MetaMask SDK?",
-    "How do you import the MetaMask SDK?",
-    "How can users connect the MetaMask SDK to their wallet?",
-    "Can the MetaMask SDK be used with a variety of web frameworks?",
-    "How do you install the MetaMask SDK?",
-    "What is the proper way of instantiating the MetaMask SDK?",
-    "What are the possible options when instantiating the MetaMask SDK?",
-    "What should be called first to make the SDK render the QR code on the console?",
-    "What is the Ethereum Provider API?",
-    "How can you view a NodeJS app example of the MetaMask SDK?",
-  ];
+    const chain = ConversationalRetrievalQAChain.fromLLM(model, retriever);
 
-  for (const question of questions) {
-    const res = await chain.call({ question, chat_history: [] });
-    const answer = res.text.trim();
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-    console.log(`Q: ${question}`);
-    console.log(`A: ${answer}`);
-    console.log("\n\n");
+    // Use rl.questionAsync() instead of util.promisify(rl.question)
+    rl.questionAsync = function (question) {
+      return new Promise((resolve) => {
+        rl.question(question, resolve);
+      });
+    };
+
+    const chatHistory = [];
+
+    while (true) {
+      const userQuestion = await rl.questionAsync(
+        "Enter your question or type 'exit' to quit: "
+      );
+
+      if (userQuestion.toLowerCase() === "exit") {
+        break;
+      }
+
+      const res = await chain.call({
+        question: userQuestion,
+        chat_history: chatHistory,
+      });
+
+      const answer = res.text.trim();
+      chatHistory.push(`${userQuestion} ${answer}`);
+
+      console.log(`Q: ${userQuestion}`);
+      console.log(`A: ${answer}\n\n`);
+    }
+
+    rl.close();
+  } catch (error) {
+    console.error(error);
   }
 }
 
-main().catch((error) => console.error(error));
+main();
