828d85f6e56b2a5b0dbfb03aa782a57ee427d7f0
Andrea Salvatore <andrea.salvatore@consensys.net>
2023-05-02 13:38:28 +0200

    adds color prompt

--- llm_setup.js
+++ llm_setup.js
@@ -1,11 +1,12 @@
 import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { loadAndProcessDocuments } from "./document_processor.js";
 import { model } from "./openAI_model.js";
+import colors from "colors";
 
 export async function llmSetup(directoryPath) {
-  console.log("Loading and processing documents...");
+  console.log(colors.red("Loading and processing documents..."));
   const vectorStore = await loadAndProcessDocuments(directoryPath);
-  console.log("Vectore store loaded and processed.\n\n");
+  console.log(colors.yellow("Vectore store loaded and processed.\n\n"));
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
