f34bee06327a776b55186103dead3330f23a7499
Anton Atanasov <anton.atanasov@consensys.net>
2023-05-13 23:24:55 +0300

    Fix docker compose and reset model

--- openAI_model.js
+++ openAI_model.js
@@ -10,7 +10,7 @@ export const model = new OpenAI({
   // modelName: "text-davinci-003", // GPT-3.5 - max 4097 tokens
   modelName: "gpt-3.5-turbo", // GPT-3.5 - max 4097 tokens
   // modelName: "gpt-4", // GPT-4 - max 8192 tokens
-  temperature: 0.2,
+  temperature: 0,
   maxTokens: 2500,
   verbose: true,
   // prompt:"" // This is the prompt that will be used for all calls to the model. You can add "personalities" here if you want to use them.
