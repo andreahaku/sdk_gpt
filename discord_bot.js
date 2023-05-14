6a1d6b687928b51ab8da670207815ac16a011eaa
Anton Atanasov <anton.atanasov@consensys.net>
2023-05-14 16:05:27 +0300

    Sync discord propmpts

--- discord_bot.js
+++ discord_bot.js
@@ -59,18 +59,20 @@ client.on("messageCreate", async (message) => {
         break;
 
       default:
-        if (!content.startsWith("!askMM")) return;
+        if (!content.startsWith("!askTEMP")) return;
 
         const question = content.slice(7).trim();
         const authorId = message.author.id;
         let isTyping = true;
-        await sendMessageTyping(message, isTyping);
+        await message.channel.sendTyping();
 
         try {
           // Get the chat history for this user
           const userChatHistory = chatHistory.get(authorId) || [];
 
-          const promptTemplate = "You are an expert in MetaMask documentation. Make sure you give an extended and detailed answer. Provide code snippets every time it's possible and makes sense to do so.";
+          // const promptTemplate = "You are an expert in MetaMask documentation. Make sure you give an extended and detailed answer. Provide code snippets every time it's possible and makes sense to do so.";
+          const promptTemplate = "You are an expert in MetaMask documentation. Please answer the following question if you know the answer give a precise and detailed answer. If you don't know the answer say that you don't know. Provide code snippets every time when possible.";
+
           const msg = `${promptTemplate} \n ${question}`
 
           const chain = await chainPromise;
